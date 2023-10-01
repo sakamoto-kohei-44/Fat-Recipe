e92e0e8b1b4e76fbee8c8982b444d5b2ea9f446c
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-10-02 07:36:24 +0900

    :sparkles:マイグレーションファイルの設定#8

---
+++ db/migrate/20231001222528_create_favorites.rb
@@ -0,0 +1,10 @@
+class CreateFavorites < ActiveRecord::Migration[7.0]
+  def change
+    create_table :favorites do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :recipe, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
