549e4024fab943ce5461508655dd5bb68b3cb997
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-10-06 19:55:35 +0900

    :sparkles:ユーザー情報入力フォーム作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def body_info
+    @user = User.find(params[:id]) # idはURLのパラメーターから取得することを想定しています
+    if @user.update(user_params)
+      # 成功した場合の処理
+      redirect_to some_path, notice: "体の情報を更新しました"
+    else
+      # 失敗した場合の処理
+      render 'users/body_info'
+    end
   end
 
   def gender_age
@@ -17,12 +25,6 @@ class UsersController < ApplicationController
   def edit_profile
   end
 
-  def update_profile
-  end
-
   def edit_account
-  end
-
-  def update_account
   end
 end
