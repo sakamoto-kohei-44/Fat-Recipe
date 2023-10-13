12b9c89e50107deefcad1a13543a2059f8353f91
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-10-13 21:39:11 +0900

    :+1:レシピ作成ボタン設置

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.autoload_paths += %W(#{config.root}/app/services)
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
