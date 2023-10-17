ab0dc0d5ad48f0ca983a2ada4d7787598c75e374
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-10-17 21:41:38 +0900

    👕 コードの品質とスタイルの修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link styles.css
 //= link scripts.js
 //= link application.js
-//= link profile_icon.css
-//= link bootstrap.js
+//= link application.css
