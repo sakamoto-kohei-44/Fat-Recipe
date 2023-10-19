3a7e02d5b37478acf213953d84d526c2b033329e
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-10-19 13:27:20 +0900

    :sparkles:翻訳APIの実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,16 @@ class RecipesController < ApplicationController
     user = current_user || User.new(name: "Guest")
     cache_key = "meal_plan_for_user_#{user.id || 'guest'}"
     @meal_plan = Rails.cache.fetch(cache_key, expires_in: 1.hour) do
-      SpoonacularService.generate_meal_plan
+      meal_plan = SpoonacularService.generate_meal_plan
+      # meals の各要素の title を翻訳
+      meal_plan["meals"].each do |meal|
+        original_title = meal["title"]
+        translated_title = SpoonacularService.translate_text(meal["title"], 'en', 'ja')
+        Rails.logger.info "Original title: #{original_title}"
+        Rails.logger.info "Translated title: #{translated_title}"
+        meal["title"] = translated_title
+      end
+      meal_plan
     end
   end
 
