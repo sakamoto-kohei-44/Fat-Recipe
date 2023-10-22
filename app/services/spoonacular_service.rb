ebc959f8a54e5ae09c9dd3a82f7110a2b20630f6
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-10-22 17:53:13 +0900

    :sparkles:必要カロリー取得

--- app/services/spoonacular_service.rb
+++ app/services/spoonacular_service.rb
@@ -30,19 +30,6 @@ class SpoonacularService
     end
   end
 
-  def self.fetch_multiple_recipe_information(ids)
-    # 複数のレシピ情報を一度に取得するためのAPIエンドポイントは提供されていないかもしれません。
-    # そのため、一つ一つのレシピ情報を順次取得して結果を配列に格納する方法を採用します。
-    recipes_info = []
-
-    ids.each do |id|
-      recipe_info = fetch_recipe_information(id)
-      recipes_info << recipe_info unless recipe_info.empty?
-    end
-
-    recipes_info
-  end
-
   def self.fetch_recipe_information(id)
     url = "#{BASE_URL}/recipes/#{id}/information"
     response = HTTParty.get(url, query: { apiKey: API_KEY, includeNutrition: true })
