6bb93e79eceea00399ba640e1cc3a368d0d15614
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-11-02 20:02:45 +0900

    :sparkles:sorcery追加による修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ApplicationRecord
+  authenticates_with_sorcery!
+  include Sorcery::Model
   attr_accessor :skip_special_validation
-  before_validation :set_skip_special_validation
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }, unless: :skip_special_validation?
+  validates :height, :weight, presence: true, numericality: { greater_than: 0 }, unless: :skip_special_validation?
+
+  validates :email, uniqueness: true, presence: true
   has_many :weight_logs
   has_many :recipes
   has_many :food_preferences
   has_many :allergies
   has_many :favorites
-
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }, unless: :skip_special_validation?
-  validates :height, :weight, presence: true, numericality: { greater_than: 0 }, unless: :skip_special_validation?
-
 
   enum gender: { man: 0, woman: 1 }
   enum goal: { standard: 0, slim_muscle: 1 }
@@ -21,9 +23,5 @@ class User < ApplicationRecord
 
   def skip_special_validation?
     skip_special_validation == true
-  end
-
-  def set_skip_special_validation
-    self.skip_special_validation = true if new_record? && password.present? && email.present?
   end
 end
