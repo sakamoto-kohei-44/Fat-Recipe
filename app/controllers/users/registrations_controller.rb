a3e3d3039406937ceca41db9ae0407a75fc25b13
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-11-02 20:05:05 +0900

    不要ファイル

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-module Users
-  class RegistrationsController < Devise::RegistrationsController
-    def create
-      build_resource(sign_up_params)
-      resource.skip_special_validation = true
-      super
-    end
-  end
-end
