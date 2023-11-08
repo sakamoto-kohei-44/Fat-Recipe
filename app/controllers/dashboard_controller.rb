f4e7a92cfefaaca4cc6f61efa84916a640875dd7
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-11-09 00:54:05 +0900

    :sparkle javascriptの設定

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   def index
+    @user = current_user
+    # ユーザーの体重記録を取得
+    @weight_logs = @user.weight_logs
+    # グラフ用データを作成
+    @weight_data = @weight_logs.pluck(:date, :weight)
   end
 end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,6 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+import Chartkick from "chartkick"
+window.Chartkick = Chartkick
+import "chartkick/chart.js"
