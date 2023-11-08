40b50667874448ecad8f61cba42b0b8d7cf88fc3
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-11-08 09:40:14 +0900

    :sparkles:セッションをログイン時にDBに保存する実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_31_081355) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_173235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_31_081355) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "name"
     t.integer "gender"
     t.integer "age"
@@ -75,14 +74,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_31_081355) do
     t.float "weight"
     t.integer "activity_level"
     t.integer "goal"
-    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.float "target_weight"
     t.string "crypted_password"
     t.string "salt"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "avatar"
+    t.text "allergy_item_ids", default: [], array: true
+    t.float "tdee"
+    t.float "bmr"
+    t.float "target_calorie"
+    t.string "email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
