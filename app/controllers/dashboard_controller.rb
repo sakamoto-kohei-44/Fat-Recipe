3a140129df2d1c684bfe4861c3fb6d7d3675db51
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-11-10 17:23:15 +0900

    :sparkles:グラフのリアルタイム更新機能実装中

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
   def index
     if logged_in?
       @chart_data = current_user.weight_logs.pluck(:date, :weight)
-      @chart_data = current_user.weight_logs.pluck(:date, :weight)
       @chart_data.map! do |data|
         [data[0].strftime("%Y/%m/%d"), data[1]]
       end
