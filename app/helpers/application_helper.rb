c7a3af12e8c1195e1315e90b07cd2c349b9d9a71
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-11-18 20:12:23 +0900

    :sparkles: ログイン画面のフラッシュメッセージの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     case message_type
     when 'notice' then 'bg-green-100 border border-green-400 text-green-700'
     when 'alert'  then 'bg-red-100 border border-red-400 text-red-700'
-    when 'error'  then 'bg-red-100 border border-red-400 text-red-700'
+    when 'dander'  then 'bg-red-300 border border-red-400 text-red-700'
     when 'info'   then 'bg-blue-100 border border-blue-400 text-blue-700'
     else 'bg-gray-100 border border-gray-400 text-gray-700'
     end
