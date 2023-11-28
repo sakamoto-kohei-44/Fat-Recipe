5e3b2126d4cc4e272f276b33bbd8a199b2284f75
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-11-28 19:29:52 +0900

    デプロイ修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
+  config.serve_static_assets = true
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
