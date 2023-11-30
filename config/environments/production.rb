030236b04641eeb2c0026640b0517edbc28a10e3
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-11-30 18:16:33 +0900

    :+1: production.rbの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.assets.css_compressor = nil
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.serve_static_assets = true
 
