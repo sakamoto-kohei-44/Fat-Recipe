1946856fef01768f6e421bcddaed57cee24ae05c
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-12-31 16:54:10 +0900

    :+1: recipesコントローラーのリファクタリング

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,19 @@
 class RecipesController < ApplicationController
+  before_action :set_services
   def index
     if params[:calories].blank?
       flash.now[:alert] = t('.fail')
       render 'recipe_suggestions/index'
       nil
     else
-      open_ai_service = OpenAiService.new
-      deepl_service = DeepLService.new
-      allergies = AllergyItem.where(id: session[:allergy_item_ids]).pluck(:name) if session[:allergy_item_ids].present?
+      allergies = []
+      if session[:allergy_item_ids].present?
+        allergies = AllergyItem.where(id: session[:allergy_item_ids]).pluck(:name)
+      end
       free_word = params[:free_word]
-      Rails.logger.info "Free word received: #{free_word}"
       disliked_foods = params[:disliked_foods]
-      @recipe = open_ai_service.generate_recipe(params[:calories], allergies, disliked_foods, free_word)
-      @translated_recipe = deepl_service.translate(@recipe, "JA")
+      @recipe = @open_ai_service.generate_recipe(params[:calories], allergies, disliked_foods, free_word)
+      @translated_recipe = @deepl_service.translate(@recipe, "JA")
 
       parse_recipes(@translated_recipe)
     end
@@ -34,26 +35,8 @@ class RecipesController < ApplicationController
 
   def search_results
     deepl_service = DeepLService.new
-    queries = params[:query].split(/,\s*/)
-    @recipes = []
-    queries.each do |query|
-      translated_query = deepl_service.translate(query, "EN")
-      next if translated_query.blank?
-
-      response = SpoonacularService.search(query: translated_query)
-      if response["error"]
-        logger.error "Response body: #{response.body}"
-      else
-        response["results"].each do |recipe|
-          translated_title = deepl_service.translate(recipe["title"])
-          @recipes << {
-            id: recipe["id"],
-            title: translated_title,
-            image: recipe["image"]
-          }
-        end
-      end
-    end
+    translated_queries = translate_queries(params[:query], deepl_service)
+    @recipes = search_and_transform_recipes(translated_queries, deepl_service)
     render :search_results
   end
 
@@ -84,5 +67,31 @@ class RecipesController < ApplicationController
   def extract_meal(recipe_text, start_index, end_index)
     return nil unless start_index
     recipe_text[start_index...end_index].strip
+  end
+
+  def set_services
+    @open_ai_service = OpenAiService.new
+    @deepl_service = DeepLService.new
+  end
+
+  def translate_queries(query_string, deepl_service)
+    query_string.split(/,\s*/).map do |query|
+      deepl_service.translate(query, "EN")
+    end.reject(&:blank?)
+  end
+
+  def search_and_transform_recipes(queries, deepl_service)
+    queries.each_with_object([]) do |query, recipes|
+      response = SpoonacularService.search(query: query)
+      next if response["error"]
+      response["results"].each do |recipe|
+        translated_title = deepl_service.translate(recipe["title"])
+        recipes << {
+          id: recipe["id"],
+          title: translated_title,
+          image: recipe["image"]
+        }
+      end
+    end
   end
 end
