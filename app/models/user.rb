3e181625f7c42de572f00592fbc54719c8cecc30
sakamoto-kohei-44 <banbengongping111@gmail.com>
2024-01-03 16:27:54 +0900

    :+1: パスワード入力フォームに文字数制限を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   attr_accessor :skip_special_validation
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
