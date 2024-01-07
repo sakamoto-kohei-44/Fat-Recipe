b58479af3377ef8fd6ce3571250c66c173d3df71
sakamoto-kohei-44 <banbengongping111@gmail.com>
2024-01-08 00:01:20 +0900

    :bug: アレルギー未入力時のバグ修正

--- app/models/allergies_form.rb
+++ app/models/allergies_form.rb
@@ -4,4 +4,10 @@ class AllergiesForm
   attr_accessor :allergy_item_ids
 
   validates :allergy_item_ids, presence: true
+
+  private
+
+  def allergy_items_present
+    errors.add(:allergy_item_ids, :blank) if allergy_item_ids.blank? || allergy_item_ids.reject(&:blank?).empty?
+  end
 end
