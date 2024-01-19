f88ee7023d19eb9e159eb15033b42fcb3c3726f6
sakamoto-kohei-44 <banbengongping111@gmail.com>
2024-01-19 10:20:54 +0900

    :+1: ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,6 @@ Rails.application.routes.draw do
 
   get '/home', to: 'pages#dashboard', as: 'dashboard_home'
   get 'recipe_suggestions/index', as: 'recipe_suggestions'
-  get 'recipes/search'
-  get 'recipes/search_results'
   patch 'users/update_profile', to: 'users#update_profile'
   patch 'users/update_account', to: 'users#update_account'
 
@@ -31,12 +29,12 @@ Rails.application.routes.draw do
       get 'height_weight_target_weight'
       get 'activity_level'
       get 'allergies'
+      get 'confirmation', to: 'users#confirmation', as: 'confirmation'
       post 'save_goal', to: 'users#save_goal', as: 'save_goal' #ユーザーが目標（標準体型または細マッチョ）を選択したときに、その情報を保存するためのアクションを実行するためのもの
       post 'save_gender_age', to: 'users#save_gender_age', as: 'save_gender_age' #ユーザーの年齢、性別
       post 'save_height_weight_target_weight', to: 'users#save_height_weight_target_weight', as: 'save_height_weight_target_weight'
       post 'save_activity_level', to: 'users#save_activity_level', as: 'save_activity_level'
       post 'save_allergies', to: 'users#save_allergies', as: 'save_allergies'
-      get 'confirmation', to: 'users#confirmation', as: 'confirmation'
       # アカウント設定関連
       get 'account_settings', to: 'users#edit_account', as: 'edit_account'
     end
@@ -49,7 +47,7 @@ Rails.application.routes.draw do
   resource :profile, only: %i[show edit update]
 
   # ダッシュボード
-  get 'dashboard', to: 'dashboard#index', as: 'dashboard'
+  resources :dashboard, only: [:index]
 
   # レシピ関連
   resources :recipes, only: [:index, :show] do
